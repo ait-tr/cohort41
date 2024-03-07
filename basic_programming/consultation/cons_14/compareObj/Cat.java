fc2e9184d3c017fa6fdf9d44ff6f737bc3e34776
StanislavPB <perminovsb@gmail.com>
2024-03-07 15:43:19 +0000

    add consultation 14

---
+++ basic_programming/consultation/cons_14/compareObj/Cat.java
@@ -0,0 +1,31 @@
+package compareObj;
+
+public class Cat implements Comparable<Cat>{
+    String name;
+    int age;
+
+    public Cat(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Cat otherCat) {
+        //return this.name.compareTo(otherCat.name);
+        int compareResult = otherCat.age - this.age;
+
+        if (compareResult == 0) {
+            compareResult = otherCat.name.compareTo(this.name);
+        }
+
+        return compareResult;
+    }
+}
---
+++ basic_programming/consultation/cons_14/compareObj/CompareArrays.java
@@ -0,0 +1,25 @@
+package compareObj;
+
+import java.util.Arrays;
+
+public class CompareArrays {
+    public static void main(String[] args) {
+        int[] ints = {6,3,8,1,4,9,2,5,7};
+
+        System.out.println(Arrays.toString(ints));
+
+        Arrays.sort(ints);
+
+        System.out.println(Arrays.toString(ints));
+
+
+        String[] strings = {"b","d","e","a","c","g","f"};
+        System.out.println(Arrays.toString(strings));
+
+        Arrays.sort(strings);
+
+        System.out.println(Arrays.toString(strings));
+
+
+    }
+}
