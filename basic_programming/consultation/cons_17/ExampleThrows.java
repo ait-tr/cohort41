322e14812be0b6c3d897c481c9664eeabf0b4880
StanislavPB <perminovsb@gmail.com>
2024-03-19 15:04:18 +0000

    add consultation 17

---
+++ basic_programming/consultation/cons_17/ExampleThrows.java
@@ -0,0 +1,28 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class ExampleThrows {
+    public static void main(String[] args) {
+
+        try {
+            readFile();
+        } catch (FileNotFoundException e){
+           e.printStackTrace();
+        }
+
+
+    }
+
+
+    public static void readFile() throws FileNotFoundException {
+
+        FileReader fileReader = new FileReader("text.txt");
+
+    }
+
+
+
+
+
+}
